21f40b4da2bb463154b2914deac61716f0011345
dariovazano <dario.vazano@hotmail.com>
2023-05-02 06:06:52 +0200

    funcion pintar terminada

--- app.js
+++ app.js
@@ -5,15 +5,15 @@
 const categories= ["Vivienda","Alimentación","Transporte"]
 
 const bills= [
-    {categorie:"Vivienda", coment:"vivienda", bill:1750},
-    {categorie:"Vivienda", coment:"vivienda", bill:75},
-    {categorie:"Vivienda", coment:"vivienda", bill:7},
-    {categorie:"Alimentación", coment:"Alimentación", bill:850},
-    {categorie:"Alimentación", coment:"Alimentación", bill:75},
-    {categorie:"Alimentación", coment:"Alimentación", bill:7},
-    {categorie:"Transporte", coment:"Transporte", bill:950},
-    {categorie:"Transporte", coment:"Transporte", bill:75},
-    {categorie:"Transporte", coment:"Transporte", bill:7}
+    {categorie:"Vivienda", fecha:"22/04/2023", coment:"vivienda", bill:1750},
+    {categorie:"Vivienda", fecha:"22/04/2023", coment:"vivienda", bill:75},
+    {categorie:"Vivienda", fecha:"22/04/2023", coment:"vivienda", bill:7},
+    {categorie:"Alimentación", fecha:"22/04/2023", coment:"Alimentación", bill:850},
+    {categorie:"Alimentación", fecha:"22/04/2023", coment:"Alimentación", bill:75},
+    {categorie:"Alimentación", fecha:"22/04/2023", coment:"Alimentación", bill:7},
+    {categorie:"Transporte", fecha:"22/04/2023", coment:"Transporte", bill:950},
+    {categorie:"Transporte", fecha:"22/04/2023", coment:"Transporte", bill:75},
+    {categorie:"Transporte", fecha:"22/04/2023", coment:"Transporte", bill:7}
 ]
 
 
@@ -43,23 +43,35 @@ function pintar(){
     });
     //tarjetas
     categories.forEach(categorie =>{
+        //Creo la tarjeta
         let tarjeta = document.createElement("div")
         tarjeta.className = "col-12 col-xl-3 col-md-6"
         tarjeta.innerHTML=
-        `
-        <div class="card  text-bg-secondary mb-3">
-            <div class="card-body">
-                <h5 class="card-title">${categorie}</h5>
-                <ul id="${categorie}"class="list-group list-group-flush">
-                  <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">22/03/2023 - alq <span class="badge bg-primary rounded-pill">$750</span> <button type="button" class="btn btn-danger btn-sm">-</button></li>
-                  <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">22/03/2023 - alq <span class="badge bg-primary rounded-pill">$750</span> <button type="button" class="btn btn-danger btn-sm">-</button></li>
-                  <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">22/03/2023 - alq <span class="badge bg-primary rounded-pill">$750</span> <button type="button" class="btn btn-danger btn-sm">-</button></li>
-                  <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"><a href="#" class="btn btn-success">+</a></li>
-              </ul>
-              </div>
-        </div>
-    `
-    document.querySelector("#categories").appendChild(tarjeta)
+            `
+                <div class="card  text-bg-secondary mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">${categorie}</h5>
+                        <ul id="${categorie}"class="list-group list-group-flush">
+                    </ul>
+                    </div>
+                </div>
+            `
+        document.querySelector("#categories").appendChild(tarjeta)
+        //creo el listado en cada tarjeta
+        let resultados = bills.filter((producto) => producto.categorie === categorie)
+        for (let resultado of resultados){
+        let cardList=document.createElement("div")
+        cardList.className="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+        cardList.innerHTML=
+            `
+            ${resultado.fecha} - ${resultado.coment} <span class="badge bg-primary rounded-pill">$${resultado.bill}</span> <button type="button" class="btn btn-danger btn-sm">-</button>
+            `
+            document.querySelector(`#${categorie}`).appendChild(cardList)
+        }
+
+        
+
+
 
 
     });
