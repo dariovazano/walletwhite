29787250c863e3f6f52160033611561df8a5590f
dariovazano <dario.vazano@hotmail.com>
2023-05-18 18:29:12 +0200

    tercera entrega v0

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Saludo inicial
 
 
-// const categories = ["Vivienda", "Alimentación", "Transporte"]
 
-// const bills = [
+// const categoriesreset = ["Vivienda", "Alimentación", "Transporte"]
+
+// const billsreset = [
 //     { id: 1, categorie: "Vivienda", fecha: "22/04/2023", coment: "vivienda", bill: 1750 },
 //     { id: 2, categorie: "Vivienda", fecha: "22/04/2023", coment: "vivienda", bill: 75 },
 //     { id: 3, categorie: "Vivienda", fecha: "22/04/2023", coment: "vivienda", bill: 7 },
@@ -15,14 +16,13 @@
 //     { id: 9, categorie: "Transporte", fecha: "22/04/2023", coment: "Transporte", bill: 7 }
 // ]
 
-
 const categories = []
 const bills = []
 
 
 
 
-// localStorage.setItem("data", JSON.stringify(local))
+
 
 //compruebo si hay algo en el local storage.
 
@@ -30,7 +30,6 @@ let data = localStorage.getItem("data")
 let local = JSON.parse(data)
 console.log(local)
 if (local == null) {
-    console.log("aqui")
     local = {
         "categories": categories,
         "bills": bills,
@@ -75,6 +74,7 @@ function pintar() {
     //tarjetas
 
     local.categories.forEach(categorie => {
+
         //cuento cuantos gastos hay en esa categoria
         let n = local.bills.filter((producto) => producto.categorie === categorie).length
 
@@ -115,8 +115,49 @@ function pintar() {
             document.querySelector(`#${categorie}`).appendChild(cardList)
         }
 
+
     });
 }
+
+
+
+//funcion pintar formulario
+
+//creo las opciones en el formulario
+
+function pintarform(cat) {
+    // Pinto el formulario de las categorias 
+    local.categories.forEach(categorie => {
+        let options = document.createElement("option")
+        if (cat == categorie) {
+            options.setAttribute("selected", "")
+        }
+        options.innerHTML = `${categorie}`
+        document.querySelector("#select").appendChild(options)
+    })
+    // pinto el formulario de las fechas 
+
+    let hoy = new Date()
+    let day = hoy.getDate()
+    let month = hoy.getMonth() + 1
+
+    // Le doy el formato al mes como lo requiere el imput yyyy-mm-dd 
+    let monthb = ""
+    if (month < 10) { monthb = `0${month}` }
+
+    let year = hoy.getFullYear()
+
+
+    let fecha = document.createElement("div")
+    fecha.innerHTML = ` <label for="fechas" class="form-label">Fechas del gasto</label>
+    <input type="date" id="inputfecha" name="trip-start" value="${year}-${monthb}-${day}" min="1990-01-01" max="2050-12-31">
+    `
+    document.querySelector("#formufechas").appendChild(fecha)
+
+}
+
+
+
 
 //creo la funcion de reseteo
 const reset = () => {
@@ -129,52 +170,74 @@ const reset = () => {
         const padre1 = document.querySelector("#categories");
         const hijo1 = padre1.lastChild;
         padre1.removeChild(hijo1)
+        //borro todo lo del formulario
+        const padre2 = document.querySelector("#select");
+        const hijo2 = padre2.lastChild;
+        padre2.removeChild(hijo2)
     })
+    // borro el formulario de la fecha 
+    const padre3 = document.querySelector("#formufechas");
+    const hijo3 = padre3.lastChild;
+    padre3.removeChild(hijo3)
 }
 
 
 pintar()
+pintarform()
+
+
 
 
 
 //Eventos
 
 //Agregar categoría
-const agregarCat = document.querySelector("#agregarCategoria")
-agregarCat.addEventListener("click", (e) => {
+const formulario1 = document.querySelector("#agregarCategoria")
+const nuevacat = document.querySelector("#categorianueva")
+formulario1.addEventListener("submit", (e) => {
+    e.preventDefault()
     reset()
-    console.log(e.target.name)
-    local.categories.push(prompt("La nueva categoria es:"))
+    local.categories.push(nuevacat.value.replace(" ", "-"))
     pintar()
-
+    pintarform()
     localStorage.setItem("data", JSON.stringify(local))
+    formulario1.reset()
 });
 
-//agregar Gasto
+//agregar Gasto y Eliminar Gasto
+
+//este evento captura el boton de donde se criqueo, 
+//si es agregar gasto solo te scrollea hasta la seccion donde se agrega y te completa la categoria en el fomulario
+//si es en un botoncito de eliminar elimina el gasto directamente.
+
 const agregargasto = document.querySelector("#categories")
 agregargasto.addEventListener("click", (e) => {
     if (e.target.name == "add") {
-
         reset()
-
-        const nuevoGasto = {}
-        nuevoGasto.id = local.bills.length + 1
-
-        nuevoGasto.categorie = e.target.id//id es la categoria en este caso
-        nuevoGasto.fecha = prompt("fecha")
-        nuevoGasto.coment = prompt("comentario")
-        nuevoGasto.bill = Number(prompt("gasto"))
-
-        // nuevoGasto.fecha="dddd"
-        // nuevoGasto.coment="dfdfsdf"
-        // nuevoGasto.bill=333
-
-
-        local.bills.push(nuevoGasto)
-
         pintar()
-        localStorage.setItem("data", JSON.stringify(local))
+        pintarform(e.target.id)
+        // scroll hasta el formuilario 
+        const element = document.getElementById("cargarGasto");
+        element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
     }
+
+    //     reset()
+
+    //     const nuevoGasto = {}
+    //     nuevoGasto.id = local.bills.length + 1
+
+    //     nuevoGasto.categorie = e.target.id//id es la categoria en este caso
+    //     nuevoGasto.fecha = prompt("fecha")
+    //     nuevoGasto.coment = prompt("comentario")
+    //     nuevoGasto.bill = Number(prompt("gasto"))
+
+
+    //     local.bills.push(nuevoGasto)
+
+    //     pintar()
+    //     pintarform()
+    //     localStorage.setItem("data", JSON.stringify(local))
+    // }
     if (e.target.name == "remove") {
         reset()
         console.log("   hola")
@@ -189,16 +252,59 @@ agregargasto.addEventListener("click", (e) => {
                 i = i + 1
             }
         })
-
-
         pintar()
+        pintarform()
         localStorage.setItem("data", JSON.stringify(local))
     }
+})
+//aqui esta el evento para capturar el gasto añadido
+const formugasto = document.querySelector("#formugasto")
+let categoriagasto = document.querySelector("#select")
+console.log(categoriagasto.value)
+let fechagasto = document.querySelector("#inputfecha")
+let gastogasto = document.querySelector("#inputgasto")
+const comentariogasto = document.querySelector("#inputcomentario")
+formugasto.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log("adentro")
+    let aux = categoriagasto.value
+    reset()
+
+    let nuevoGasto = {}
+    nuevoGasto.id = local.bills.length + 1
+    nuevoGasto.categorie = aux
+    nuevoGasto.fecha = fechagasto.value
+    nuevoGasto.coment = comentariogasto.value
+    nuevoGasto.bill = Number(gastogasto.value)
+    console.log(nuevoGasto)
+
+    local.bills.push(nuevoGasto)
+
+    pintar()
+    pintarform()
+    localStorage.setItem("data", JSON.stringify(local))
 
 
 })
 
-//eliminar gasto
+
+
+
+
+//reset
+
+const back = document.querySelector("#borrarMemoria")
+back.addEventListener("click", (e) => {
+    reset()
+    local.categories = []
+    local.bills = []
+
+
+
+    console.log(local)
+    pintar()
+    localStorage.setItem("data", JSON.stringify(local))
+})
 
 
 
